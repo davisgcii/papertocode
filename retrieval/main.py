74002f9e0b011d845421dee726fa759d1ef52321
iyaja <ajayuppili@gmail.com>
2023-02-25 15:56:24 -0800

    chore: push

---
+++ retrieval/main.py
@@ -0,0 +1,38 @@
+from langchain.vectorstores import Chroma
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.llms import OpenAI
+from langchain.chains import VectorDBQA
+from langchain.document_loaders import TextLoader
+from langchain.document_loaders import OnlinePDFLoader
+
+def get_context(arxiv_link: str, prompt: str) -> str:
+
+    # Load the document
+    loader = OnlinePDFLoader()
+    doc = loader.load(arxiv_link)
+
+    # Split the document into sentences
+    splitter = RecursiveCharacterTextSplitter()
+    sentences = splitter.split(doc)
+
+    # Embed the sentences
+    embeddings = OpenAIEmbeddings()
+    embedded_sentences = embeddings.embed(sentences)
+
+    # Create a vector store
+    store = Chroma()
+
+    # Create a language model
+    lm = OpenAI()
+
+    # Create a QA chain
+    chain = VectorDBQA(store, lm)
+
+    # Add the embedded sentences to the vector store
+    for sentence, embedding in zip(sentences, embedded_sentences):
+        store.add(sentence, embedding)
+
+    # Ask the QA chain a question
+    return chain.ask(prompt)
+
