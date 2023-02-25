f061b0ef18d71421e4cbd2c70581d5961b1b52af
George Davis <davisgcii@gmail.com>
2023-02-25 15:36:25 -0800

    made frontend prettier

--- server/server.py
+++ server/server.py
@@ -5,22 +5,7 @@ app = Flask(__name__)
 # Route to serve the HTML page with the text form
 @app.route('/')
 def form():
-    return '''
-        <!DOCTYPE html>
-        <html>
-          <head>
-            <title>Text Form</title>
-          </head>
-          <body>
-            <h1>Enter the Arxiv Paper's url</h1>
-            <form action="/api/endpoint" method="post">
-              <label for="text-input">url:</label><br>
-              <input type="text" id="text-input" name="text"><br><br>
-              <input type="submit" value="Submit">
-            </form>
-          </body>
-        </html>
-    '''
+    return render_template('text_form.html')
 
 # Route to handle the form submission and send the text to the API endpoint
 @app.route('/api/endpoint', methods=['POST'])
